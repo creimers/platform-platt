17a8678fee5262a120b99555d2b019c256d65eb8
Christoph Reimers <christoph@superservice-international.com>
2016-09-10 11:56:30 +0200

    implement role filter

--- restapp/src/apps/account/viewsets.py
+++ restapp/src/apps/account/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -5,9 +6,16 @@ from .models import Account as User
 from .serializers import UserPublicSerializer
 
 
+class UsersFilter(filters.FilterSet):
+    class Meta:
+        model = User
+        fields = ['role', ]
+
+
 class UsersViewset(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsAuthenticated, )
     serializer_class = UserPublicSerializer
+    filter_class = UsersFilter
 
     def get_queryset(self):
         return User.objects.exclude(id=self.request.user.id)
