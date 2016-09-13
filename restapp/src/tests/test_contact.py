2aed7c6d8d888e2ecb271af716c763a57e34fca8
Christoph Reimers <christoph@superservice-international.com>
2016-09-13 08:22:24 +0200

    test posting to contacts endpoint

--- restapp/src/tests/test_contact.py
+++ restapp/src/tests/test_contact.py
@@ -12,3 +12,42 @@ def test_contact_POST_401(rest_client):
     url = reverse('contacts-list')
     response = rest_client.post(url)
     assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_contact_POST_201(rest_client, users):
+    """
+    post a new contact with existing users
+    """
+    sender = users[0]
+    receiver = users[1]
+
+    new_contact = {
+        'sender': sender.id,
+        'receiver': receiver.id,
+        'message': "Justo Inceptos Porta Bibendum"
+    }
+
+    rest_client.force_authenticate(user=sender)
+    url = reverse('contacts-list')
+    response = rest_client.post(url, new_contact)
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_contact_POST_400(rest_client, users):
+    """
+    post a new contact non with existing users
+    """
+    sender = users[0]
+
+    new_contact = {
+        'sender': sender.id,
+        'receiver': 12000,
+        'message': "Justo Inceptos Porta Bibendum"
+    }
+
+    rest_client.force_authenticate(user=sender)
+    url = reverse('contacts-list')
+    response = rest_client.post(url, new_contact)
+    assert response.status_code == 400
